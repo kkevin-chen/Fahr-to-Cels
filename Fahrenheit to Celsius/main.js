2f30c64eb57f1eeaf5abf5e5e9ed66d23df1f558
kkevin-chen <52505438+kkevin-chen@users.noreply.github.com>
2019-07-06 16:23:48 -0600

    Add files via upload

---
+++ Fahrenheit to Celsius/main.js
@@ -0,0 +1,19 @@
+//Fahrenheit to Celsius JavaScript
+
+// Event Listener
+document.getElementById('submit').addEventListener('click', celsiusConverter);
+
+// Event Function
+function celsiusConverter() {
+
+    // Input
+    let fahrTemp = Number(document.getElementById('fahrInput').value);
+
+    //Processing
+    let celsTemp = (fahrTemp-32)*(5/9);
+    let sentence = fahrTemp + '°F is equal to ' + celsTemp + '°C.'
+
+    //Output
+    document.getElementById('output-title').innerHTML = 'Output:';
+    document.getElementById('output-sentence'). innerHTML = sentence;
+}
